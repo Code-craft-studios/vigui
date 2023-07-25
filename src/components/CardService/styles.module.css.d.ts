370fda96e89a097e184a9c4003b8fb043d1caf4e
Patrick <patricioalmada911@gmail.com>
2023-07-25 20:52:31 -0300

    refactor: benefits section

--- src/components/CardService/styles.module.css.d.ts
+++ src/components/CardService/styles.module.css.d.ts
@@ -2,6 +2,7 @@ declare const styles: {
   readonly "colorPrimary": string;
   readonly "colorSecondary": string;
   readonly "price": string;
+  readonly "title": string;
   readonly "card": string;
   readonly "list": string;
   readonly "textBody": string;
