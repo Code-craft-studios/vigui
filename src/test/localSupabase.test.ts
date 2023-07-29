c20c767a45deeebf60d1b7dd4c1d3beae772e4a7
shadow <pieroramirez810@gmail.com>
2023-07-29 16:15:59 -0400

    test: mock fetch response supabase

--- src/test/localSupabase.test.ts
+++ src/test/localSupabase.test.ts
@@ -1,14 +1,18 @@
-import { describe, test, expect, vi, type MockedFunction } from 'vitest'
-import { createFetchResponse } from './utils/fetch'
+import createFetchMock, { FetchMock } from 'vitest-fetch-mock';
+
+import { describe, test, expect, vi } from 'vitest'
+
 import { LocalSupabase } from '@/lib/services/supabase'
 import { createClient } from '@supabase/supabase-js'
 
-const globalFetch = vi.fn()
 
-global.fetch = globalFetch
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
 
-const supabaseClient = createClient('https://pablito.com', 'frr43fr2rfvfv24rf24rgg324rf42')
-const supbaseLocalClient = new LocalSupabase(supabaseClient);
+const SUPABASE_URL = 'https://pablito.com'
+const SUPBASE_KEY = 'frr43fr2rfvfv24rf24rgg324rf42'
+const supabaseClient = createClient(SUPABASE_URL, SUPBASE_KEY)
+const supabaseLocalClient = new LocalSupabase(supabaseClient);
 
 
 describe('supabase service', ()=>{
@@ -18,11 +22,15 @@ describe('supabase service', ()=>{
       title: 'adasdsad'
     }];
 
-    (fetch as typeof globalFetch)
-      .mockResolvedValue(createFetchResponse(response))
+    const fetchMock = (fetch as FetchMock)
+      .mockResponse(JSON.stringify(response))
 
-    const responseSupa = await supbaseLocalClient.fetchTestData();
+    const responseSupa = await supabaseLocalClient.fetchTestData();
+    
+    const [request] = fetchMock.requests()
 
+    expect(request.url).toContain(SUPABASE_URL)
+    expect(request.headers.get('apiKey')).toBe(SUPBASE_KEY)
     expect(responseSupa).toStrictEqual(response);
   })
 })
